6ef51197c65a7e33e819cc61e9c1507dbbf79df6
vanessavps <nessinha.vps@gmail.com>
2025-03-11 20:48:59 +1300

    Cursor animation and colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,33 @@ $( document ).ready(function() {
 		$(this).find('a').addClass('active');
 	});
 });
+
+$(document).ready(function() {
+	$("a[href^='#']").on("click", function(event) {
+		event.preventDefault();
+		var target = $(this.getAttribute("href"));
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top -80
+			}, 800);
+		}
+	});
+});
+
+document.addEventListener("mousemove", function(event) {
+	const flashlight = document.getElementById("flashlight");
+	flashlight.style.setProperty("--x", event.clientX + "px");
+	flashlight.style.setProperty("--y", event.clientY + "px");
+});
+
+/*$(window).on("scroll", function() {
+	let scrollPosition = $(window).scrollTop();
+	$("section").each(function() {
+		let sectionTop = $(this).offset().top - 500;
+		let sectionId = $(this).attr("id");
+		if (scrollPosition >= sectionTop) {
+			$("nav ul li a").removeClass("active");
+			$("nav ul li a[href='#" + sectionId + "']").addClass("active");
+		}
+	});
+});*/
